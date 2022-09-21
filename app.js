8da95b9b4dd0d623ae8ecfcc9d5f9a411fa8724f
Cleophas <cleophasfoy@gmail.com>
2022-09-21 10:19:28 -0500

    added styling to grid

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  for (let i = 0; i < 200; i++) {
+    const div = document.createElement('div');
+
+    document.getElementById('main').appendChild(div);
+  }
+
+
+
+
+})
